30491da619720fde517ffd619805b70a6827d45b
Barry Peddycord III <ishara@isharacomix.org>
2013-12-29 00:02:58 -0500

    Made the livestream load faster and stop with the tiny zero box when there is nothing streaming.

--- live/ttycast.js
+++ live/ttycast.js
@@ -1,8 +1,15 @@
 
-window.onload = function() {
-
-  var el = document.getElementById('terminal')
+  var el = document.getElementById('term')
     , buf = new DisplayBuffer(el)
+
+
+  buf["setRows"].apply(buf, [24])
+  for ( var i = 0; i < 24; i++ )
+  {
+      buf["setCols"].apply(buf, [i,80])
+      buf["draw"].apply(buf, [0,0,"",[]])
+  }
+
 
   var socket = io.connect("ws://live.isharacomix.org:13377")
   socket.on('data', function(operations) {
@@ -12,4 +19,4 @@ window.onload = function() {
     }
   })
 
-}
+
