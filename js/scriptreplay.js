d39817793fb6490bdc2945f9a140a6ce8a1791d0
Barry Peddycord III <ishara@isharacomix.org>
2013-12-29 17:05:29 -0500

    Launching the new blog.

---
+++ js/scriptreplay.js
@@ -0,0 +1,109 @@
+/*
+script-replay in javascript
+
+(C) 2011 Johannes 'josch' Schauer <j [dot] schauer [at] email [dot] de>
+
+http://mister-muffin.de/scriptreplay/
+*/
+
+
+var vt, timer;
+var speed = 0.5;
+
+function Timer(callback, delay) {
+  var timerId, start, remaining = delay;
+
+  this.pause = function() {
+    window.clearTimeout(timerId);
+    remaining -= new Date() - start;
+  };
+
+  this.resume = function() {
+    start = new Date();
+    timerId = window.setTimeout(callback, remaining);
+  };
+
+  this.resume();
+}
+
+
+
+function play_file(name) {
+    var request = new XMLHttpRequest();
+
+    request.overrideMimeType("application/plaintext; charset=utf-8");
+    request.open("GET", name, true);
+    request.onload = function ()
+    {
+        data = JSON.parse(request.responseText);
+        
+        var cols = data["columns"] || 80;
+        var rows = data["rows"] || 24;
+        vt.resize(cols,rows);
+        var typescript_data = data["typescript"];
+        var timing_data =data["timing"];
+        run_typescript(typescript_data, timing_data);
+    };
+    request.send();
+}
+
+
+
+function play(evt) {
+
+/*  if (evt.target.textContent == "play") {
+    play_file("shelr-record.json");
+  } else if (evt.target.textContent == "resume") {
+    evt.target.textContent = "pause";
+    timer.resume();
+  } else if (evt.target.textContent == "pause") {
+    evt.target.textContent = "resume";
+    timer.pause();
+  }
+  */
+}
+
+
+function stop(evt) {
+  //document.getElementById("play").textContent = "play";
+  timer.pause();
+  //vt.clear();
+  vt.refresh();
+}
+
+function run_typescript(typescript_data, timing_data) {
+  if (timer) timer.pause();
+  //document.getElementById("play").textContent = "pause";
+  //vt.clear();
+  vt.refresh();
+
+  var where = 0;
+  var linenum = 0;
+  var timings = timing_data.split("\n");
+  var firstlinelen = typescript_data.indexOf("\n") + 1;
+  var text = typescript_data.substr(0, firstlinelen);
+  var newtext = "";
+  where += firstlinelen;
+
+  timer = new Timer(
+      function() {
+        vt.write(text);
+        text = newtext;
+        var me = arguments.callee;
+        var line = timings[linenum].split(" ");
+        var time = parseFloat(line[0]);
+        var bytes = parseInt(line[1]);
+        if (isFinite(time) && isFinite(bytes)) {
+          newtext = typescript_data.substr(where, bytes);
+          where += bytes;
+          linenum += 1;
+          timer = new Timer(me, time*1000*1/speed);
+        } else {
+          vt.write(typescript_data.substr(where, typescript_data.length-where));
+          document.getElementById("play").textContent = "play";
+        }
+      }, 0);
+}
+
+
+
---
+++ js/ttycast.js
@@ -0,0 +1,54 @@
+
+  var el = document.getElementById('term')
+    , buf = new DisplayBuffer(el)
+
+  
+  var strings = [
+"",
+"                         Welcome to IsharaComix.org!                         ",
+"",
+" This is the personal website of Barry Peddycord III. Here you can find my",
+" blog, CV, projects, and links to my other profiles around the web. Everything",
+" should be rather straightforward.",
+"",
+"",
+" Quite frequently, I livestream software development. When I'm streaming, this",
+" console window will display what is on my own terminal. I do this using",
+" websockets, meaning that older browsers will be unable to view my livestream.",
+"",
+"",
+" I can be reached via Twitter, e-mail, or IRC using the chat box below. During",
+" my streams, I'm more than happy to answer questions or hold conversations.",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+"",
+""
+  ]
+  buf["setRows"].apply(buf, [24])
+  for ( var i = 0; i < 24; i++ )
+  {
+      var s = strings[i]
+      var attr = []
+      if (s.length > 0) {
+        attr = [[3584,s.length]]
+      }
+      buf["setCols"].apply(buf, [i,80])
+      buf["draw"].apply(buf, [i,0,s,attr])
+  }
+
+
+  var socket = io.connect("ws://live.isharacomix.org:13377")
+  socket.on('data', function(operations) {
+    document.title = "IsharaComix LIVE | ON-AIR!!!"
+    for (var i = 0; i < operations.length; i ++) {
+      var operation = operations[i]
+      buf[operation[0]].apply(buf, operation.slice(1))
+    }
+  })
+
+
